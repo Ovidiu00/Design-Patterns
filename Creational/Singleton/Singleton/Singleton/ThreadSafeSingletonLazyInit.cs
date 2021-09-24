ec13f52f02adf9896df823dd12245ec0703da4e0
Ovidiu00 <63671460+Ovidiu00@users.noreply.github.com>
2021-09-24 11:59:49 +0300

    Update ThreadSafeSingletonLazyInit.cs

--- Creational/Singleton/Singleton/Singleton/ThreadSafeSingletonLazyInit.cs
+++ Creational/Singleton/Singleton/Singleton/ThreadSafeSingletonLazyInit.cs
@@ -6,7 +6,7 @@ namespace Singleton
 {
  
     /// <summary>
-    /// Double check locking
+    /// Double check locking, necesar pentru variantele mai vechi a .net , acum initalizarea eager statica este folosita pentru thread safety
     /// </summary>
     public sealed class ThreadSafeSingletonLazyInit
     {
@@ -19,9 +19,10 @@ namespace Singleton
         {
             get
             {
+             //Conditie pentru a nu creea un lock la fiecare apelare a get-ului, ci doar atunci cand nu exista deja o instanta
                 if (instance == null)
 
-                 //se folsoeste double cheking deoarece exista cazul inc are threadul este in asteparea threadului.
+                 //se folsoeste double cheking deoarece exista cazul inc are threadul este in asteparea lock-ului.
                 {
                     lock (padlock) //va bloca thread-ul current intre parantezele de la lock ,il va elibera la sfarsind permtiand urmatorului thread sa intre.
                     {
